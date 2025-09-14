4987a872f3c2ee54a254bc4d89327ae2537183a7
Ruslan_Uterly <kholikov.rus@mail.ru>
2025-09-14 10:53:21 +0300

    .

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,6 +4,8 @@ dotenv.config({ path: path.resolve(__dirname, ".env.deploy") });
 
 const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } = process.env;
 
+console.log(`${DEPLOY_PATH}`)
+
 module.exports = {
   apps: [
     {
