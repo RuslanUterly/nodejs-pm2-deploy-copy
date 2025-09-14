f34e90d545394f3b5d4dd073d3c184d9f19e5345
Ruslan_Uterly <kholikov.rus@mail.ru>
2025-09-14 10:38:50 +0300

    попробуем так

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,8 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       key: "~/.ssh/vm_access/private",
-      "pre-deploy-local": `scp -i ~/.ssh/vm_access/private ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'pre-deploy-local':
+        `ssh -i /c/Users/najmi/.ssh/vm_access/private ${DEPLOY_USER}@${DEPLOY_HOST} "mkdir -p '${DEPLOY_PATH}/current/backend' && chmod 700 '${DEPLOY_PATH}/current/backend'" && scp -i /c/Users/najmi/.ssh/vm_access/private /c/Users/najmi/Desktop/projects/nodejs-pm2-deploy/backend/.env ${DEPLOY_USER}@${DEPLOY_HOST}:'${DEPLOY_PATH}/current/backend/.env'`,
       "post-deploy":
         "cd backend && pwd && npm ci && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
     },
