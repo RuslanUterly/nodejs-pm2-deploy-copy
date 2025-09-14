8752dc45da7908895faf77cd491f6d80d1424a8d
ruslan_uterly <kholikov.rus@mail.ru>
2025-09-14 08:18:32 -0400

    .

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: __dirname + '/.env.deploy' });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      key: "~/.ssh/vm_access/private",
+      "post-deploy": 'cd frontend && pwd && npm ci && npm i && npm run build'
+    },
+  },
+}; 
