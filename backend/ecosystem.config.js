11d074af3bf8771a70909de9ec5ac9c6020cc77b
ruslan_uterly <kholikov.rus@mail.ru>
2025-09-20 07:09:01 -0400

    .

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       path: DEPLOY_PATH,
       key: "~/.ssh/vm_access/private",
       "pre-deploy-local": `scp -i ~/.ssh/vm_access/private .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
-      "post-deploy": 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
+      "post-deploy": 'source $HOME/.nvm/nvm.sh && cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
     },
   },
 }; 
