59453c2d091e80e7340d474e362da58e79f8f60f
ruslan_uterly <kholikov.rus@mail.ru>
2025-09-21 13:49:00 -0400

    .

--- backend/ecosystem.config-copy.js
+++ backend/ecosystem.config-copy.js
@@ -17,7 +17,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
-      "post-deploy": 'source $HOME/.nvm/nvm.sh && cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config-copy.js --env production'
+      "post-deploy": 'source $HOME/.nvm/nvm.sh && cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config-copy.js'
     },
   },
 }; 
