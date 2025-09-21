6d6ee01bc11e68b14851596f7f37c0a58031a674
ruslan_uterly <kholikov.rus@mail.ru>
2025-09-21 13:44:41 -0400

    for yandex cloud

---
+++ backend/ecosystem.config-copy.js
@@ -0,0 +1,27 @@
+require('dotenv').config({ path: __dirname + '/.env.deploy' });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "mesto-api",
+      script: "dist/app.js",
+      env: {
+        NODE_ENV: 'production',
+        DB_ADDRESS: 'mongodb://127.0.0.1:27017/mestodb?directConnection=true'
+      }
+    },
+  ],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
+      "post-deploy": 'source $HOME/.nvm/nvm.sh && cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
+    },
+  },
+}; 
